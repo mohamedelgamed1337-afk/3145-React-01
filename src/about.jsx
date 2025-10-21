dab43b7237d6c0143c95c4b1386b3d7c09f36955
Mohamed Sabry <mohamedelgamed1337@gmail.com>
2025-10-21 15:00:27 +0300

    made 3 clm products using only bootsrap

--- src/about.jsx
+++ src/about.jsx
@@ -1,16 +1,18 @@
-import Img from './pictures/Img.jpg'
-
+import GalaxyImg from './pictures/galaxy.jpg'
+import IphoneImg from "./pictures/iphone.jpg"
+import SamsungImg from "./pictures/Samsung.jpg"
+import "./css.css"
 export function AboutMe () {
     return (
     <div>
-    <h1>Amazoon</h1>
-    <img src={Img} alt="mohamedpic" width="300px"/>
+
+    <h1>Producttts</h1>
     <br></br>
     <ul><li><h3>About</h3></li>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate alias praesentium illEst praesentium exercitationem cumque?
                     Repellendus quasi, cumque minus est quibusdam ducimus perferendis earum tenetur assumenda tempora expedita esse illo voluptate modi facere obcaecati recusandae reprehenderit quidem suscipit voluptates quo distinctio vel? Culpa, nihil blanditiis.
                     ione labore. Quod, dolorum numquam repellendus itaque beatae cum temporibus.
-                    Necessitatibus modi consequatur atque delmo sunt consequatur sequi, libero quos debitis architecto laboriosam? Iure, libero minus.
+                    Ne  cessitatibus modi consequatur atque delmo sunt consequatur sequi, libero quos debitis architecto laboriosam? Iure, libero minus.
                     Minus et quod consequatur natus ut, nihil fuga omnis consectetur, quosmmodi repudianedit officiis nisi vitae! Necessitatibus vitae est debitis vel, nobis ipsa quidem quod recusandae! Porro nostrum illo culpa assumenda ipsum?
                     Molestiae impedit molestias exercitationemtibus aut architecto consequuntur iure fugiat!
                     Vitae consequatur natus adipisci, dolore esse possimus voluptates. Et provident repudiandae iure officia! Similique aliquam harum impedit facilis, obcaecati, explicabo atque consectetur, iure hic repudiandae ex accusamus cupiditate recusandae voluptas.
@@ -24,6 +26,46 @@ export function AboutMe () {
     <li>Product 4</li>
     </ol>
     </ul>
+  <div className='container'>
+    <div className='row g-3'>
+        <div  className='col-4'>
+          <div  className='card h-100 shadow border-danger'>
+            <div className='card-body d-flex flex-column'>
+              <div className='card-title  fw-bold fs-1'>Iphone 17 Pro</div>
+              <img  src={IphoneImg} alt='Samsunggg'/>
+              <div   className='card-title fs-2 fw-bold'>50$/<span  className='text-dark text-opacity-50'>mounth</span></div>
+              <div  className='card-text'>poor people can't buy Iphone 17, that's why poor people can't be happy</div>
+              <div    class="card-footer text-muted">Last update 3 minutes ago</div>
+              <button className='btn btn-success   w-100 mt-auto'>Add to card</button>
+        </div>
+        </div>
+        </div>
+        <div className='col-4'>
+          <div className='card h-100 shadow border-danger'>
+            <div className='card-body d-flex flex-column'>
+              <div className='card-title fs-1 fw-bold text-dark text-opacity-50'>Samsung</div>
+              <img src={SamsungImg} alt='Samsunggg' />
+              <div  className='card-title fs-2 fw-bold'>15$/<span className='text-dark text-opacity-50'>mounth</span></div>
+              <div className='card-text'>Samsung's Law "Old But Gold" it's easy to get Samsung, But Hard to Lose it</div>
+              <div    class="card-footer text-muted">Last update 3 minutes ago</div>
+              <button className='btn btn-success w-100 pt-3rem mt-auto'>Add to card</button>
+            </div>
+          </div>
+        </div>
+          <div  className='col-4'>
+            <div className='card shadow h-100 border-danger'>
+              <div  className='card-body d-flex flex-column'>
+                <div className='card-title fs-1 fw-bold text-info'>Galaxy X Cover</div>
+                <img src={GalaxyImg} alt='Galaxyyyy' style={{height:350}}/>
+                <div><span  className='text-muted text-decoration-line-through fs-2'>EGP 10000</span>  <span className='text-danger fs-2'>EGP 8000</span></div>
+                <div  className='card-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, eius.</div>
+                <div    class="card-footer text-muted">Last update 3 minutes ago</div>
+                <button className='btn btn-success w-100 mt-auto'>Add to card</button>
+              </div>
+            </div>
+          </div>
+          </div>
     </div>
+  </div>
     )
 }
