23688d812b6b7a494c0afa795e398a7e3c3aab69
Mohamed <mohamedelgamed1337@gmail.com>
2025-10-25 16:16:16 +0300

    Update TopPart.jsx

--- src/TopPart.jsx
+++ src/TopPart.jsx
@@ -3,19 +3,12 @@ import { EmptyButton } from './Buttons'
 export default function AppHeader() {
     return ( <div>
         <header>
-            
-  
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-      
       <a class="navbar-brand fw-bold fs-1 text-warning" href="text.html">AMAZOON</a>
-
-      
       <button class="navbar-toggler"  data-bs-toggle="collapse" data-bs-target="#mainNavbar">
         <span class="navbar-toggler-icon"></span>
       </button>
-
-      
       <div class="collapse navbar-collapse" id="mainNavbar">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
