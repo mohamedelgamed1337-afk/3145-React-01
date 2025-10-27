8bc749040d38c46e64a943fd911ecb3d1b537945
Mohamed Sabry <mohamedelgamed1337@gmail.com>
2025-10-27 23:23:04 +0300

    challange completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-
+import { ChallangeH1, ChallangeH2, ChallangeH3, ChallangeH4 } from './Badge'
 import Footer from './BottomPart'
 import { Appear } from './Bodyy'
 import AppHeader from './TopPart'
 export default function App() {
     return (
         <div>
-            <AppHeader></AppHeader>
-            <Appear></Appear>
-            <Footer></Footer>
+            {/* <AppHeader></AppHeader> */}
+            
+            <ChallangeH1  BgAndText1="badge bg-secondary fs-1"></ChallangeH1>
+            <ChallangeH2   BgAndText2="badge bg-primary fs-2" ></ChallangeH2>
+            <ChallangeH3    BgAndText3="badge bg-danger fs-3"></ChallangeH3>
+            <ChallangeH4    BgAndText4="badge bg-info fs-4"></ChallangeH4>
+            {/* <Appear></Appear>
+            <Footer></Footer> */}
         </div>
     )
 }
---
+++ src/Badge.jsx
@@ -0,0 +1,21 @@
+export function ChallangeH1 (badge) {
+    return (
+            <h1 className={badge.BgAndText1}>New</h1>
+    )
+    }
+ export function ChallangeH2 (badge) {
+    return (
+     <h2 className={badge.BgAndText2}>New</h2>
+    )
+}
+export function ChallangeH3 (badge) {
+        return(
+                        <h3 className={badge.BgAndText3}>New</h3>
+                    )
+    }
+export function ChallangeH4(badge) {
+    return (
+                    <h4 className={badge.BgAndText4}>New</h4>
+                )
+}
+
